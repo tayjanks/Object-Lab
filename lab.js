7c66f4347183cca5bc1f078d6cca7e86e86aaab7
tayjanks <taylorjenkins801@gmail.com>
2022-09-03 13:20:14 -0600

    problem 13

--- lab.js
+++ lab.js
@@ -214,6 +214,18 @@ console.log(cat1.name);
 
 //Code here
 
+class Wizard {
+  constructor(name, age, favoriteSpell){
+    this.name = name;
+    this.age = age;
+    this.favoriteSpell = favoriteSpell;
+  }
+  castSpell(){console.log(this.name +" has cast "+this.favoriteSpell)}
+};
+
+let wizzard1 = new Wizard ("Willow", 6, "fire ball!");
+wizzard1.castSpell();
+
 //////////////////////////// PROBLEM 14 ////////////////////////////
 /*
     Write a class called Phone. We'll use it as if we were creating
